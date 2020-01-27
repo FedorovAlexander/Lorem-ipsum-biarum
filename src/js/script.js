902fd04f7275ce4d5d087ca314af51f307030130
alex <hamster2240@gmail.com>
2020-01-28 00:15:36 +0300

    Add anchor links and smooth scroll to sections

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,15 @@ $(document).ready(function(){
     prevArrow: $('.arrow-right'),
     nextArrow: $('.arrow-left')
   });
+
+  $(document).ready(function(){
+    $(".main-nav__list").on("click","a", function(event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top - 70}, 500);
+    });
+});
 });
 
   
